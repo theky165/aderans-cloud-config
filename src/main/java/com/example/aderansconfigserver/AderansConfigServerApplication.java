5fdb988cf1f2c6dad4721a563bbd9d4ea29f541c
Ky <ky.tang@ntq-solution.com.vn>
2022-07-25 13:58:39 +0700

    base

---
+++ src/main/java/com/example/aderansconfigserver/AderansConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.aderansconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AderansConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AderansConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aderansconfigserver/AderansConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aderansconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AderansConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
